91d1a1ff9f8f0ed5e751279cd598c45384313989
Mbuso Kotobe <mbuso.kotobes@gmail.com>
2022-06-09 10:47:16 +0200

    CONFIGURATION: Added test packages and default classes.

---
+++ src/test/java/factory/EmployeeFactoryTest.java
@@ -0,0 +1,17 @@
+package factory;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeFactoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
