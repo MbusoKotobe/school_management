0f86ea72c1da46c9992952a494788af64f7cfd1d
Mbuso Kotobe <mbuso.kotobes@gmail.com>
2022-06-10 17:54:40 +0200

    ERROR FIX: Changed entry point call class from SpringBootApplication to SpringApplication.

--- src/main/java/za/ac/cput/school_management/SchoolManagementApp.java
+++ src/main/java/za/ac/cput/school_management/SchoolManagementApp.java
@@ -1,11 +1,12 @@
 package za.ac.cput.school_management;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolManagementApp {
     public static void main(String[] args)
     {
-        SpringBootApplication.run(SchoolManagementApp.class, args);
+        SpringApplication.run(SchoolManagementApp.class, args);
     }
 }
