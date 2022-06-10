18f5b6f096d24df1b3ac197af6aa214aa25d3198
Mbuso Kotobe <mbuso.kotobes@gmail.com>
2022-06-10 17:52:41 +0200

    PROJECT CONFIGURATION: Added application entry point

--- src/main/java/za/ac/cput/school_management/SchoolManagementApp.java
+++ src/main/java/za/ac/cput/school_management/SchoolManagementApp.java
@@ -4,5 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolManagementApp {
-
+    public static void main(String[] args)
+    {
+        SpringBootApplication.run(SchoolManagementApp.class, args);
+    }
 }
