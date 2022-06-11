f4b651bc3dac6ea4d60a0c234661962adf55f36a
aismail <ameerismail188@gmail.com>
2022-06-10 21:34:09 -0800

    Fix: Uncommented and removed annotations

--- src/main/java/za/ac/cput/school_management/domain/Student.java
+++ src/main/java/za/ac/cput/school_management/domain/Student.java
@@ -1,7 +1,5 @@
 package za.ac.cput.school_management.domain;
 
-import com.sun.istack.NotNull;
-import javax.persistence.*;
 
 /*
 Author: Ameer Ismail
@@ -10,15 +8,11 @@ domain: Student entity
 ADP3 June assessment Group 1
  */
 
-@Entity
 public class Student
 {
-    @Id
     private String studentId;
-
-    @NotNull
     private String email;
-    //private Name name;  -- uncomment data type when merge
+    private Name name;
 
     protected Student()
     {}
@@ -28,7 +22,7 @@ public class Student
     {
         this.studentId = builder.studentId;
         this.email = builder.email;
-        //this.name = builder.name;
+        this.name = builder.name;
     }
 
     //getters
@@ -40,27 +34,27 @@ public class Student
         return email;
     }
 
-    /*
+
     public Name getName()
     {
         return name;
     }
-     */
 
     @Override
     public String toString() {
         return "Student{" +
-                "studentId='" + studentId + '\'' +
-                ", email='" + email + '\'' +
+                "student Id ='" + studentId + '\'' +
+                ", email ='" + email + '\'' +
+                ", name =" + name +
                 '}';
-    }// add toString name
+    }
 
     // builder class
     public static class Builder
     {
         private String studentId;
         private String email;
-        //private Name name;
+        private Name name;
 
 
     // builder setters
@@ -76,19 +70,17 @@ public class Student
         return this;
     }
 
-    /*
     public Builder setName(Name name)
     {
         this.name = name;
         return this;
     }
-     */
 
     public Builder copy(Student student)
     {
         this.studentId = student.studentId;
         this.email = student.email;
-        //this.name = student.name;
+        this.name = student.name;
         return this;
     }
 
