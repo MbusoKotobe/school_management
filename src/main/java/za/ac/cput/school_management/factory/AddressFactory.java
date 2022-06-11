6ad700f58912e4fc54a1084913c062fe256b39ae
Mbuso Kotobe <mbuso.kotobes@gmail.com>
2022-06-11 16:03:53 +0200

    TASK: Implemented Address Factory

--- src/main/java/za/ac/cput/school_management/factory/AddressFactory.java
+++ src/main/java/za/ac/cput/school_management/factory/AddressFactory.java
@@ -1,4 +1,28 @@
 package za.ac.cput.school_management.factory;
 
+import za.ac.cput.school_management.domain.Address;
+import za.ac.cput.school_management.domain.City;
+import za.ac.cput.school_management.helper.Helper;
+
 public class AddressFactory {
+    public static Address createAddress(String unitNumber, String complexName, String streetNumber, String streetName, String postalCode, City city) throws IllegalArgumentException
+    {
+        validateAttributes(unitNumber, complexName, streetNumber, streetName, postalCode, city);
+        return new Address.Builder().setUnitNumber(unitNumber)
+                                    .setComplexName(complexName)
+                                    .setStreetNumber(streetNumber)
+                                    .setStreetName(streetName)
+                                    .setpostalCode(postalCode)
+                                    .setCity(city).build();
+    }
+
+    private static void validateAttributes(String unitNumber, String complexName, String streetNumber, String streetName, String postalCode, City city) throws IllegalArgumentException
+    {
+        Helper.checkStringParam("unitNumber", unitNumber);
+        Helper.checkStringParam("complexName", complexName);
+        Helper.checkStringParam("streetNumber", streetNumber);
+        Helper.checkStringParam("streetName", streetName);
+        Helper.checkStringParam("postalCode", postalCode);
+        Helper.checkObjectParams(city);
+    }
 }
