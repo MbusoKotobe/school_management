121362db9ad67946ef9dc4a1ed80feb2ccc4600c
Mbuso Kotobe <mbuso.kotobes@gmail.com>
2022-06-11 15:02:11 +0200

    TASK: Implemented EmployeeAddress Factory Test Case

---
+++ src/test/java/za/ac/cput/school_management/factory/EmployeeAddressFactoryTest.java
@@ -0,0 +1,19 @@
+package za.ac.cput.school_management.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeAddressTest {
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+}
