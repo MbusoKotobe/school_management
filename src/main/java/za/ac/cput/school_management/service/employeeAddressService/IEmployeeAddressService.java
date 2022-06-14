b4b0018389d627296420d7df2d118d034c1190f3
Mbuso Kotobe <mbuso.kotobes@gmail.com>
2022-06-14 21:20:51 +0200

    REFACTORING: Removed findAll method in IEmployeeAddressService.java

--- src/main/java/za/ac/cput/school_management/service/employeeAddressService/IEmployeeAddressService.java
+++ src/main/java/za/ac/cput/school_management/service/employeeAddressService/IEmployeeAddressService.java
@@ -6,7 +6,6 @@ import za.ac.cput.school_management.service.IService;
 import java.util.List;
 
 public interface IEmployeeAddressService extends IService<EmployeeAddress, String> {
-    List<EmployeeAddress> findAll();
     void deleteById(String employeeId);
 
 }
