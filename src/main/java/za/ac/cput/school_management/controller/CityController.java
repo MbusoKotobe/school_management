bdd6abdbbbfeb04fc52adc42cb80a73b5656ce2c
Elvis <213063964@mycput.ac.za>
2022-06-16 12:33:49 +0200

    FIXING: ERRORS on Controller class

--- src/main/java/za/ac/cput/school_management/controller/CityController.java
+++ src/main/java/za/ac/cput/school_management/controller/CityController.java
@@ -14,45 +14,55 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import za.ac.cput.school_management.domain.City;
 
-import za.ac.cput.school_management.service.CityServiceImpl;
+import za.ac.cput.school_management.factory.CityFactory;
+
+import za.ac.cput.school_management.service.employeeService.ICityService;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping ("School_Management")
+@RequestMapping ("School_Management/city/")
 @Slf4j
 public class CityController {
 
-    public CityServiceImpl cityServiceImpl;
+    private final ICityService cityServiceImpl;
 
 
     @Autowired
-    public CityController(CityServiceImpl cityServiceImpl){
+    public CityController(ICityService cityServiceImpl){
         this.cityServiceImpl = cityServiceImpl;
     }
     @PostMapping("save")
-    public ResponseEntity<City> save(@Valid @RequestBody City city){
+    public ResponseEntity<City> save(@RequestBody City city){
         log.info("save request: {}",city);
-        City save = cityServiceImpl.save(city); // should be city here
+        City valideCity;
+        try {
+            valideCity = CityFactory.createCity(city.getName(),city.getId(),city.getCountry());
+
+        }catch (IllegalArgumentException e){
+            log.info("Request for errors:{}",e.getMessage());
+            throw new ResponseStatusException((HttpStatus.BAD_REQUEST);
+        }
+        City save = cityServiceImpl.save((valideCity));
         return ResponseEntity.ok(save);
     }
     @GetMapping("read/{id}")
     public ResponseEntity<City> read (@PathVariable String id){
-        log.info("read request: {}",id);
-        City addressType = this.cityServiceImpl.read(id)
+        log.info("Read request: {}",id);
+        City city = this.cityServiceImpl.read(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        return ResponseEntity.ok(addressType);
+        return ResponseEntity.ok(city);
     }
     @DeleteMapping("delete/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id){
-        log.info("read request:{}",id);
+        log.info("delete request{}",id);
         this.cityServiceImpl.delete(id);
         return ResponseEntity.noContent().build();
     }
     @GetMapping("all")
-    public ResponseEntity<List<City>> findAll(){
-        List<City> city = (List<City>) this.cityServiceImpl.findAll();
-        return ResponseEntity.ok(city);
+    public ResponseEntity<List<City>>findAll(){
+        List<City> cityList =this.cityServiceImpl.findAll();
+        return ResponseEntity.ok(cityList);
     }
 }
