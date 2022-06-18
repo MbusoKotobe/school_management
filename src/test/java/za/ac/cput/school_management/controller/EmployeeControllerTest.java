0564d2ae37c3fb23ab2edef874e69d4c9c9b2bc5
JayKuzi <kearnsjody@gmail.com>
2022-06-18 12:52:37 +0200

    ERROR: commit to share error code

--- src/test/java/za/ac/cput/school_management/controller/EmployeeControllerTest.java
+++ src/test/java/za/ac/cput/school_management/controller/EmployeeControllerTest.java
@@ -12,11 +12,12 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import za.ac.cput.school_management.domain.Employee;
-import za.ac.cput.school_management.domain.Name;
-import za.ac.cput.school_management.factory.EmployeeFactory;
-import za.ac.cput.school_management.factory.NameFactory;
+import za.ac.cput.school_management.domain.*;
+import za.ac.cput.school_management.factory.*;
+
 import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -67,7 +68,7 @@ class EmployeeControllerTest {
         );
     }
 
-    @Order(3)
+    @Order(5)
     @Test
     void delete(){
         String url = baseUrl + "delete/" + this.employee.getStaffId();
@@ -75,7 +76,7 @@ class EmployeeControllerTest {
         this.restTemplate.delete(url);
     }
 
-    @Order(4)
+    @Order(6)
     @Test
     void findAll(){
         String url = baseUrl + "all";
@@ -86,6 +87,40 @@ class EmployeeControllerTest {
         assertAll(
                 () -> assertEquals(HttpStatus.OK, response.getStatusCode()),
                 () -> assertEquals(0, response.getBody().length)
+        );
+    }
+
+    //Question 5 Test:
+    @Order(3)
+    @Test
+    void findByEmail(){
+        String url = baseUrl + "read-by-email/" + this.employee.getEmail();
+        System.out.println(url);
+        ResponseEntity<Name> response =
+                this.restTemplate.getForEntity(url, Name.class);
+        System.out.println(response.getBody());
+        assertAll(
+                () -> assertEquals(HttpStatus.OK, response.getStatusCode()),
+                () -> assertNotNull(response.getBody())
+        );
+    }
+
+    @Order(4)
+    @Test
+    void findEmpByCity(){
+        Country country = CountryFactory.build("USA", "United States of America");
+        City city = CityFactory.build("TX","Texas", country);
+        Address address = AddressFactory.build("4395", "Bluebell Village","102","Excelsior Street","7558",city);
+        EmployeeAddress employeeAddress = EmployeeAddressFactory.build("209023651", address);
+        String url = baseUrl + "read-employee-name-by-city-id/" + employeeAddress.getAddress().getCity().getId();
+        System.out.println(url);
+
+        ResponseEntity<Name[]> response =
+                this.restTemplate.getForEntity(url, Name[].class);
+        System.out.println(Arrays.asList(response.getBody()));
+        assertAll(
+                () -> assertEquals(HttpStatus.OK,response.getStatusCode()),
+                () -> assertEquals(1, response.getBody().length)
         );
     }
 }
